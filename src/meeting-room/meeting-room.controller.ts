fc2bf9fc743a93aa7b87f40aefff5b5caef70cac
NYou <luqianyou117@163.com>
2025-05-25 22:21:30 +0800

    会议室相关管理

--- src/meeting-room/meeting-room.controller.ts
+++ src/meeting-room/meeting-room.controller.ts
@@ -1,7 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { MeetingRoomService } from './meeting-room.service';
+import { generateParseIntPipe } from 'src/utils';
+import { CreateMeetingRoomDto } from './dto/create-meeting-room.dto';
+import { UpdateMeetingRoomDto } from './dto/update-meetiong-room.dto';
 
 @Controller('meeting-room')
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) {}
+
+  @Get('init')
+  initData() {
+    return this.meetingRoomService.initData();
+  }
+
+  @Get('list')
+  async list(
+    @Query('pageNo', new DefaultValuePipe(1), generateParseIntPipe('pageNo'))
+    pageNo: number,
+    @Query(
+      'pageSize',
+      new DefaultValuePipe(2),
+      generateParseIntPipe('pageSize'),
+    )
+    pageSize: number,
+  ) {
+    return await this.meetingRoomService.find(pageNo, pageSize);
+  }
+
+  @Post('create')
+  async create(@Body() meetingRoomDto: CreateMeetingRoomDto) {
+    return await this.meetingRoomService.create(meetingRoomDto);
+  }
+
+  @Put('updata')
+  async update(@Body() meetingRoomDto: UpdateMeetingRoomDto) {
+    return await this.meetingRoomService.update(meetingRoomDto);
+  }
+
+  @Get(':id')
+  async find(@Param('id') id: number) {
+    return await this.meetingRoomService.findById(id);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    return await this.meetingRoomService.delete(id);
+  }
 }
--- src/meeting-room/meeting-room.module.ts
+++ src/meeting-room/meeting-room.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MeetingRoomService } from './meeting-room.service';
 import { MeetingRoomController } from './meeting-room.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MeetingRoom } from './entities/meeting-room.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MeetingRoom])],
   controllers: [MeetingRoomController],
   providers: [MeetingRoomService],
 })
